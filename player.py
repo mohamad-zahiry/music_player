3bd7a537444c9e13095c9be56ce526863eb9dfa6
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-05-30 12:56:19 +0430

    Creating audioPlayer core

---
+++ player.py
@@ -0,0 +1,25 @@
+import vlc
+
+
+class Player:
+    def __init__(self):
+        self.player = vlc.MediaPlayer()
+
+    def __getattribute__(self, name: str):
+        try:
+            return super().__getattribute__(name)
+        except AttributeError:
+            return self.player.__getattribute__(name)
+
+    def set_audio(self, path):
+        self.player = vlc.MediaPlayer(path)
+
+    def set_equalizer(self, n: int):
+        p_equalizer = vlc.AudioEqualizer()
+        p_equalizer.set_preamp(n)
+        self.player.set_equalizer(p_equalizer)
+
+    def get_position_percent(self):
+        if self.player.is_playing():
+            return round(self.player.get_position() * 100)
+        return 0
